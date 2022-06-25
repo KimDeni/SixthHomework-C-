d04b3d3282de133e9ea5ef1c8e8ac3de47783c25
Denis Kim <denis.kim666@gmail.com>
2022-06-25 16:35:00 +0600

    Homework added

---
+++ SecondTask/Program.cs
@@ -0,0 +1,25 @@
+﻿void binary(int num){
+
+    int counter = 0;
+    int temp = num;
+
+        while (temp != 0){
+        temp = temp /2;
+        counter++;
+    }
+
+    int[] arr = new int[counter];
+
+    for(int i = arr.Length - 1; i >= 0; i--){
+        arr[i] = num % 2;
+        num = num /2;
+    }
+
+    for( int  i = 0; i < arr.Length; i++){
+        Console.Write(arr[i]);
+    }
+
+}
+int number = Convert.ToInt32(Console.ReadLine());
+
+binary(number);
---
+++ TirthTask/Program.cs
@@ -0,0 +1,23 @@
+﻿void coordinates()
+{
+    System.Console.Write("Введите x1: ");
+    double x1 = Convert.ToDouble(Console.ReadLine());
+    System.Console.Write("Введите y1: ");
+    double y1 = Convert.ToDouble(Console.ReadLine());
+    System.Console.Write("Введите x2: ");
+    double x2 = Convert.ToDouble(Console.ReadLine());
+    System.Console.Write("Введите y2: ");
+    double y2 = Convert.ToDouble(Console.ReadLine());
+
+    intersectiPoint(x1, y1, x2, y2);
+}
+
+void intersectiPoint(double x1, double y1, double x2, double y2)
+{
+    double x = (x2-x1)/(y1-y2);
+    double y = (y2*x) + x2;
+    System.Console.WriteLine($"{"Точка пересечения: "} {x} {":"} {y}");
+}
+
+
+coordinates();
